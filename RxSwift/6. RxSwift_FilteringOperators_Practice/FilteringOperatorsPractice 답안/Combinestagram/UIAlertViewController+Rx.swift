dd1e9f381428a6eadef158526928efda9fdbad65
수꿍 <99063327+Jeon-Minsu@users.noreply.github.com>
2023-01-27 18:18:30 +0900

    Add files via upload

---
+++ RxSwift/6. RxSwift_FilteringOperators_Practice/FilteringOperatorsPractice 답안/Combinestagram/UIAlertViewController+Rx.swift
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+import RxSwift
+
+// MARK: 6-1. Alert을 Observable화 후, extension으로 별도 구현
+// UIAlertController를 생성
+// UIAlertAction은 닫기 기능만을 제공
+
+extension UIViewController {
+    func alert(title: String, text: String?) -> Observable<Void> {
+        return Observable.create { [weak self] observer in
+            let alertVC = UIAlertController(title: title, message: text, preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "Close", style: .default, handler: { _ in
+                observer.onCompleted()
+
+                self?.dismiss(animated: true, completion: nil)
+
+            }))
+            self?.present(alertVC, animated: true, completion: nil)
+
+            return Disposables.create()
+        }
+    }
+}
