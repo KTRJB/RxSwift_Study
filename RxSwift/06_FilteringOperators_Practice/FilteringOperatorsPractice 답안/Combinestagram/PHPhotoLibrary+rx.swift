5a46f8e9e57fe481a684c628ed1430ca069cd23f
수꿍 <99063327+Jeon-Minsu@users.noreply.github.com>
2023-01-27 18:28:45 +0900

    Add files via upload

---
+++ RxSwift/06_FilteringOperators_Practice/FilteringOperatorsPractice 답안/Combinestagram/PHPhotoLibrary+rx.swift
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import Foundation
+import Photos
+import RxSwift
+
+// MARK: 4. 사진 라이브러리 접근 권한 설정 Observable
+// 접근을 허용하였으면 true 값을 생성후 완료
+// 이외의 경우 우선 false 값을 생성 후
+// 접근 권한에 대한 사용자 선택을 요청한 다음, 사용자 선택에 따른 bool 값을 생성 후 완료
+
+extension PHPhotoLibrary {
+  static var authorized: Observable<Bool> {
+    return Observable.create { observer in
+
+      DispatchQueue.main.async {
+        if authorizationStatus() == .authorized {
+          observer.onNext(true)
+          observer.onCompleted()
+        } else {
+          observer.onNext(false)
+          requestAuthorization { newStatus in
+            observer.onNext(newStatus == .authorized)
+            observer.onCompleted()
+          }
+        }
+      }
+
+      return Disposables.create()
+    }
+  }
+}
